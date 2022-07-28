-Fixed issue with Eq calcs (not squaring thetasq), match results now -Added a block for applying multiple layers of MCS instead of averaging, an improvement -[WIP] Started function for filterings, but will need improvement!

examples/EricScripts/plotFit.py
@@ -136,6 +136,16 @@ def findFit(data):
   #return the mean, abs(sigma), interval #sigma can sometimes be - so must abs() it.
   return(popt[0],abs(popt[2]),x_interval_for_fit) #for Vac and Air sigma sometimes give - number...
 
+def filterMe(Filterer,toFilter1,toFilter2,toFilter3,toFilter4,criteria,value):
+  if criteria == ">"
+    gfilt = np.greater(Filterer,value)
+    fltrd1 = toFilter1[gfilt]
+    fltrd2 = toFilter2[gfilt]
+    fltrd3 = toFilter3[gfilt]
+    fltrd4 = toFilter4[gfilt]
+
+  return fltrd1,fltrd2,fltrd3,fltrd4
+
 def calcTwiss(labelx,labely,x,y):
   #use generic x,y but usually x, x' or y,y'!
   import numpy as np
@@ -164,10 +174,11 @@ def calcEq8(thetasq,Twiss,thick,beta_rel,gamma_rel):
   e8dgem = 0.5 * Twiss[0]*m * thetasq * thetasq #[m*rad^2]
   e8alph = Twiss[2] * Twiss[1] / (Twiss[2] + e8dgem)
   e8beta = Twiss[2] * Twiss[0]*m / (Twiss[2] + e8dgem) #[m]
-  e8gamma = (Twiss[2] * Twiss[3] + thetasq ) / (Twiss[2] + e8dgem) #[m^-1]
+  e8gamma = (Twiss[2] * Twiss[3] + thetasq * thetasq ) / (Twiss[2] + e8dgem) #[m^-1]
   e8gemt = Twiss[2] + e8dgem
   #e8nemt = e8nemt/(beta_rel*gamma_rel)
-  print("e8",e8dgem,e8beta,e8alph,e8gemt,e8gamma)
+  print("e8",thetasq,e8beta,e8alph,e8gemt,e8gamma)
+  #28.7-supposed to have thetasq*thetasq in gamma. Previously did NOT have it! Now numbers are great!
   return [e8beta,e8alph,e8gemt,e8gamma]
 
 def calcEq16(thetasq,Twiss,thick,beta_rel,gamma_rel):
@@ -179,18 +190,18 @@ def calcEq16(thetasq,Twiss,thick,beta_rel,gamma_rel):
   #Twiss=[beta,alpha,gemt,gamma]
   #Calculations from Eq 15 and 16 from Twiss MCS Formalism Calculations from https://cds.cern.ch/record/499590
   e16dgem = 0.5 * thetasq * thetasq * (Twiss[0]*m + thick*mm * Twiss[1] + thick*mm*thick*mm/3 * Twiss[3]) #[m*rad^2]
-  e16alph = (Twiss[2] * Twiss[1] - thick*mm * 0.5 * thetasq) / (Twiss[2] + e16dgem)
-  e16beta = (Twiss[2] * Twiss[0]*m + thick*mm * thick*mm / 3 * thetasq) / (Twiss[2] + e16dgem) #[m]
-  e16gamma = (Twiss[2] * Twiss[3] + thetasq ) / (Twiss[2] + e16dgem) #[m^-1]
+  e16alph = (Twiss[2] * Twiss[1] - thick*mm * 0.5 * thetasq* thetasq ) / (Twiss[2] + e16dgem)
+  e16beta = (Twiss[2] * Twiss[0]*m + thick*mm * thick*mm / 3 * thetasq* thetasq ) / (Twiss[2] + e16dgem) #[m]
+  e16gamma = (Twiss[2] * Twiss[3] + thetasq * thetasq ) / (Twiss[2] + e16dgem) #[m^-1]
   e16gemt = Twiss[2]*um + e16dgem
-  #e16nemt = e16nemt*(beta_rel*gamma_rel)
+  #28.7-supposed to have thetasq*thetasq in gamma, alph and beta! Previously did NOT have it! Now numbers are great!
   return [e16beta,e16alph,e16gemt,e16gamma]
 
 def getMoments(Twiss):
   from numpy import sqrt
   mm=1e-3
   #[beta,alpha,gemt,gamma]
-  sigma = sqrt(Twiss[0]*Twiss[2]) /mm #sigma x rms = beam size = sqrt(beta*epsG)
+  sigma = sqrt(Twiss[2]*Twiss[0]) /mm #sigma x rms = beam size = sqrt(beta*epsG)
   sigmapx = sqrt(Twiss[2]*Twiss[3]) #mrad actually
   #print(sigma,sigmapx,sigpx)
   return [sigma,sigmapx]
@@ -218,19 +229,18 @@ def plotTwissFit(xs,pxs,savename,mat,titledescr,axis,thick,thetasq,beta_rel,gamm
   #print(Mcal,Me8,Me16)
 
   print("Recalculated Twiss:","Betax: {:.2f}m, alphax: {:.2f}, Geo Emitt x: {:.3e}m".format(calcTwf[0],calcTwf[1],calcTwf[2]))
-  #print("PDF")
-  print("Calculated  sigmaTwx: {:.2f}mm, sigmaTwX': {:.2f}mrad".format(Mcal[0],Mcal[1]/mm))
-  print("Equation 8  sigmaTwx: {:.2f}mm, sigmaTwX': {:.2f}mrad".format(Me8[0],Me8[1]/mm))
-  print("Equation 16 sigmaTwx: {:.2f}mm, sigmaTwX': {:.2f}mrad".format(Me16[0],Me16[1]/mm))
 
   #Get intervals and info about distribution
   mux,sigmax,xinterval = findFit(xs)
   mupx,sigmapx,pxinterval = findFit(pxs) #this is a least square fit to Gaussian, so less sensitive to outliers.
   print("Histogram   sigmaTwx: {:.2f}mm, sigmaTwX': {:.2f}mrad".format(sigmax,sigmapx/mm))
+  print("Calculated  sigmaTwx: {:.2f}mm, sigmaTwX': {:.2f}mrad".format(Mcal[0],Mcal[1]/mm))
+  print("Equation 8  sigmaTwx: {:.2f}mm, sigmaTwX': {:.2f}mrad".format(Me8[0],Me8[1]/mm))
+  print("Equation 16 sigmaTwx: {:.2f}mm, sigmaTwX': {:.2f}mrad".format(Me16[0],Me16[1]/mm))
 
   #Create the fig with 2 plots side by side
   plt.clf()
-  fig = plt.figure(figsize=(15,6.0))
+  fig = plt.figure(figsize=(16,6.0))
   plt.subplots_adjust(wspace=0.25) #increase width space to not overlap
   s1 = fig.add_subplot(1,2,1)
   s2 = fig.add_subplot(1,2,2)
@@ -243,12 +253,12 @@ def plotTwissFit(xs,pxs,savename,mat,titledescr,axis,thick,thetasq,beta_rel,gamm
     y1a = norm.pdf(binsx, mux, sigmax)
     l1a = s1.plot(binsx, y1a, "k--", linewidth=1,label="Filtered "+axis+" Histogram PDF") #least Square
     y1b = norm.pdf(binsx, mux, Mcal[0])
-    l1b = s1.plot(binsx, y1b, "r--", linewidth=2,label="Filtered "+axis+" Twiss PDF") #RMS
+    l1b = s1.plot(binsx, y1b, "r--", linewidth=2,label="Filtered "+axis+" Twiss RMS PDF") #RMS
 
     y2a = norm.pdf(binspx, mupx, sigmapx)
     l2a = s2.plot(binspx, y2a, "k--", linewidth=1,label="Filtered "+axis+"' Histogram PDF")
     y2b = norm.pdf(binspx, mupx, Mcal[1])
-    l2b = s2.plot(binspx, y2b, "r--", linewidth=2,label="Filtered "+axis+"' Twiss PDF")
+    l2b = s2.plot(binspx, y2b, "r--", linewidth=2,label="Filtered "+axis+"' Twiss RMS PDF")
 
     order = [2,0,1]#for no Eq fits for initial Hist plot
   else:
@@ -256,7 +266,7 @@ def plotTwissFit(xs,pxs,savename,mat,titledescr,axis,thick,thetasq,beta_rel,gamm
     y1a = norm.pdf(binsx, mux, sigmax)
     l1a = s1.plot(binsx, y1a, "k--", linewidth=1,label="Filtered "+axis+" Histogram PDF")
     y1b = norm.pdf(binsx, mux, Mcal[0])
-    l1b = s1.plot(binsx, y1b, "r--", linewidth=2,label="Filtered "+axis+" Twiss PDF")
+    l1b = s1.plot(binsx, y1b, "r--", linewidth=2,label="Filtered "+axis+" Twiss RMS PDF")
     y1c = norm.pdf(binsx, mux, Me8[0])
     l1c = s1.plot(binsx, y1c, "b.", linewidth=1,label=PDFlabele8+" "+axis+" Twiss PDF")
     y1d = norm.pdf(binsx, mux, Me8[0])
@@ -265,7 +275,7 @@ def plotTwissFit(xs,pxs,savename,mat,titledescr,axis,thick,thetasq,beta_rel,gamm
     y2a = norm.pdf(binspx, mupx, sigmapx)
     l2a = s2.plot(binspx, y2a, "k--", linewidth=1,label="Filtered "+axis+"' Histogram PDF")
     y2b = norm.pdf(binspx, mupx, Mcal[1])
-    l2b = s2.plot(binspx, y2b, "r--", linewidth=2,label="Filtered "+axis+"' Twiss PDF")
+    l2b = s2.plot(binspx, y2b, "r--", linewidth=2,label="Filtered "+axis+"' Twiss RMS PDF")
     y2c = norm.pdf(binspx, mupx, Me8[1])
     l2c = s2.plot(binspx, y2c, "b.", linewidth=1,label=PDFlabele8+" "+axis+"' Twiss PDF")
     y2d = norm.pdf(binspx, mupx, Me16[1])

examples/EricScripts/simulation.py
@@ -16,18 +16,18 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
   #constants for below use
   #particle characteristic values
   if beam == "proton":
-    partmass = 938.27209 #[MeV/c2]
-    z=1
+    partA = 938.27209 #[MeV/c2]
+    partZ = 1
   elif beam == "electron":
-    partmass = 0.511 #[MeV/c2]
-    z=1
+    partA = 0.511 #[MeV/c2]
+    partZ = 1
   c = 2.99792e8 #[m/s]
   MeV = 1e6*1.602e-19 
   um = 1e-6 #[m] #need to convert to real units as the equations use real units.
   m = 1 #[m]
   mm = 1e-3 #[m]
   ummrad = um*1e-3
-  gamma_rel = 1 + energy/partmass #from PrintTwissParameters
+  gamma_rel = 1 + energy/partA #from PrintTwissParameters
   beta_rel = np.sqrt(gamma_rel*gamma_rel -1 )/gamma_rel
 
   #Get rid of Normalized Emittance!
@@ -93,15 +93,23 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
   if material == "G4_Galactic":
     mat = "Vac"
     radLen = 1e12 #[mm] basically infinity
+    atomZ = 0.1
+    atomA = 0.1
   elif material == "G4_Al":
     mat = "Al"
     radLen = 88.97 #[mm] #0.2401 #[g/mm^2] # #24.01 [g/cm^2]
+    atomZ = 13
+    atomA = 26.981
   elif material == "G4_AIR":
     mat = "Air"
     radLen = 3.122e5 #[mm] -taken from 80% N2 gas(3.260e5mm) and 20% O2 gas (2.571e5mm)
+    atomZ = 2 #the lbl page says Z/A = 0.49919, so 2/4 is close enough
+    atomA = 4
   elif material == "G4_Au":
     mat = "Au"
     radLen = 3.344
+    atomZ = 79
+    atomA = 196.966
 
   if thick!=0.0:
     baseSimSetup["THICK"] = thick
@@ -120,7 +128,7 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
     #How to construct a magnet for miniScatterDriver, as per kyrsjo/MiniScatter/blob/master/examples/SiRi DeltaE-E detector.ipynb
     #Al first piece
     m1 = {}
-    m1["pos"]      = 0.6250
+    m1["pos"]      = 0.6250 #pos is at CENTER
     m1["type"]     = "TARGET"
     m1["length"]   = 1.2500 #[mm]
     m1["gradient"] = 0.0
@@ -131,7 +139,7 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
     baseSimSetup["MAGNET"].append(m1)
     #Water
     m2 = {}
-    m2["pos"]      = 2.2501
+    m2["pos"]      = 2.2501 #pos is at CENTER
     m2["type"]     = "TARGET"
     m2["length"]   = 2.0000 #[mm]
     m2["gradient"] = 0.0
@@ -142,7 +150,7 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
     baseSimSetup["MAGNET"].append(m2)
     #Al Back
     m3 = {}
-    m3["pos"]      = 3.7502
+    m3["pos"]      = 3.7502 #pos is at CENTER
     m3["type"]     = "TARGET"
     m3["length"]   = 1.000 #[mm]
     m3["gradient"] = 0.0
@@ -153,8 +161,11 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
     baseSimSetup["MAGNET"].append(m3)
 
     mat = "Real"
-    #radLen = 88.97 #modeling all Al contribution
+    radLenAl = 88.97 #Al
+    radLenH2O = 36.08 #liquid Water 
     radLen = 64.08 #mm average of 2.25mm Al and 2.0mm Light Water
+    atomZ = 13 + 8 + 2 / 4 #not great, but ok
+    atomA = 26.9 + 16 + 2 / 4
     #from https://cds.cern.ch/record/1279627/files/PH-EP-Tech-Note-2010-013.pdf
     #radLen = 4.25/((.225*2.70)/8.897 + (2.0*1.0)/3.608) 
 
@@ -216,10 +227,13 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
       Einit[i] = myTree.E
   myFile.Close()
 
-  initxTw = calcTwiss("Initial X","Initial X'",xinit,pxinit)
-  inityTw = calcTwiss("Initial Y","Initial Y'",yinit,pyinit)
+  #initxTw = calcTwiss("Initial X","Initial X'",xinit,pxinit)
+  #inityTw = calcTwiss("Initial Y","Initial Y'",yinit,pyinit)
 
   #Filter with Energy for now as PDG isn't working yet
+  from plotFit import filterMe
+
+  #xinit_filtered, pxinit_filtered, yinit_filtered, pyinit_filtered = filterMe(Einit,xinit,pxinit,yinit,pyinit,">",energy*Engcut)
   Einit_filter = np.greater(Einit,energy*Engcut)
   xinit_filtered = xinit[Einit_filter]
   pxinit_filtered = pxinit[Einit_filter]
@@ -266,8 +280,8 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
       PDGexit[i] = myTree.PDG
   myFile.Close() 
 
-  exitxTw = calcTwiss("Exit X","Exit X'",xexit,pxexit)
-  exityTw = calcTwiss("Exit Y","Exit Y'",yexit,pyexit)
+  #exitxTw = calcTwiss("Exit X","Exit X'",xexit,pxexit)
+  #exityTw = calcTwiss("Exit Y","Exit Y'",yexit,pyexit)
 
   #Filter the relevant distributions
   #print(len(Eexit),len(PDGexit),len(xexit))
@@ -396,28 +410,30 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
   #Display Full Energy distribution results
   print("Full Energy distribution of {:d} particles with minimum Energy {:.3f}MeV through ".format(len(Eexit),np.min(Eexit_filtered)),mat," PBW")
 
-  #get Twiss parameters from tracker at ESS Target location:
-  #twiss, numPart,objects = miniScatterDriver.getData(savedfile)
-  #Tnemx = twiss["tracker_cutoffPDG2212"]["x"]["eps"] *um #[um]
-  #Tbetax = twiss["tracker_cutoffPDG2212"]["x"]["beta"] *m #[m]
-  #Talphx = twiss["tracker_cutoffPDG2212"]["x"]["alpha"] #[um-mrad]
-  #Tnemy = twiss["tracker_cutoffPDG2212"]["y"]["eps"] *um #[um]
-  #Tbetay = twiss["tracker_cutoffPDG2212"]["y"]["beta"] *m
-  #Talphy = twiss["tracker_cutoffPDG2212"]["y"]["alpha"] #[um-mrad]
-
   #Highland Equation Radiation Length Calculation
-  p = np.sqrt((energy+partmass)**2 - (partmass)**2) #[MeV/c] #derived with Kyrre 15.6.22
+  p = np.sqrt((energy+partA)**2 - (partA)**2) #[MeV/c] #derived with Kyrre 15.6.22
   betap = beta_rel*p #Eq 5
-  thetasq = 13.6 * z / betap * np.sqrt(thick/radLen) * (1 + 0.038 * np.log(thick/radLen)) #from Eq 5
-  #q=partmass*partmass/energy #used for mimicing AnaScatter's "BeamBeta2" RadLen Calculatons
+  thetasq = 13.6 * partZ / betap * np.sqrt(thick/radLen) * (1 + 0.038 * np.log(thick/radLen)) #from Eq 5
+  #q=partA*partA/energy #used for mimicing AnaScatter's "BeamBeta2" RadLen Calculatons
   #betap = energy-q 
-  #thetasq = 13.6 * z / betap * np.sqrt(thick/radLen) * (1 + 0.038 * np.log(thick*z*z/(radLen*(1-q/energy)))) #from MiniScatter
+  #thetasq = 13.6 * partZ / betap * np.sqrt(thick/radLen) * (1 + 0.038 * np.log(thick*partZ**2/(radLen*(1-q/energy)))) #from MiniScatter
   print("\nradLen: {:.2f}, p: {:.3e}, gamma: {:.3f}, beta: {:.3f}, theta^2: {:.3e} radians".format(radLen,p,gamma_rel,beta_rel,thetasq))
   #using beta*q or energy-q produces similar #s (within 7%)
 
-  #Plotting PBW Exit distribution vs the PDF produced from the Formalism Equations
-  from plotFit import plotTwissFit,calcEq8,calcEq16
+  #14/15 June 22 - Section for Characteristic and Screening Angle calculations, 
+  # as per https://doi-org.ezproxy.uio.no/10.1016/0168-583X(91)95671-Y
+  #MUST find a way to have multiple layers of contribution for real PBW!
+  centerFrac = [0.7,0.9,0.95,0.98,0.99,0.995,0.997]
+  characAng = 0.157 * (atomZ * (atomZ+1) * thick/atomA) * (partZ / (p * beta_rel))**2
+  screenAng = 2.007e-5 * (atomZ**(2.0/3)) * (1 + 3.34 * (atomZ * partZ * (1/137) / beta_rel)**2 ) / (p*p)
+  meanScatters = characAng / screenAng
+  nu = 0.5 * meanScatters / (1-centerFrac[3]) 
+  sigma = characAng / (1-centerFrac[3]**2) * ((1+nu)/nu * np.log(1+nu) -1)
+  print("For fraction {}, characteristic angle {:0.2e}, screening angle {:0.2e}".format(centerFrac[3],characAng,screenAng))
+  print("We get {:0.2e} scatters, nu {:0.2e} and sigma {:0.2e}".format(meanScatters,nu,sigma))
 
+  #Do multiple scattering layers instead of averaging!
+  from plotFit import plotTwissFit,calcEq8,calcEq16
   #Use list of Twiss values for simple passing of data: [beta,alpha,gemt]
   TwissIx   = [Ibetax,Ialphx,Igemx*um,((1+Ialphx*Ialphx)/Ibetax)] #Initial Twiss
   TwissIy   = [Ibetay,Ialphy,Igemy*um,((1+Ialphy*Ialphy)/Ibetay)]
@@ -428,32 +444,56 @@ def simulation(N,material,beam,thick,Inemx,Inemy,Ialphx,Ialphy,Ibetax,Ibetay,ene
   Twisse16x = calcEq16(thetasq,TwissIx,thick,beta_rel,gamma_rel) #calculated 2 target exit Twiss
   Twisse16y = calcEq16(thetasq,TwissIy,thick,beta_rel,gamma_rel)
 
+  if baseSimSetup["THICK"] == 0.0:
+    m1Len = baseSimSetup["MAGNET"][0]["length"]
+    #Al Front contribution
+    thetasqAl1 = 13.6 * partZ / betap * np.sqrt(m1Len/radLenAl) * (1 + 0.038 * np.log(m1Len/radLenAl))
+    Twisse8xAl1 = calcEq8(thetasqAl1, TwissIx,m1Len,beta_rel,gamma_rel)
+    Twisse8yAl1 = calcEq8(thetasqAl1, TwissIy,m1Len,beta_rel,gamma_rel)
+
+    #H2O contribution
+    m2Len = baseSimSetup["MAGNET"][1]["length"]
+    thetasqH2O = 13.6 * partZ / betap * np.sqrt(m2Len/radLenH2O) * (1 + 0.038 * np.log(m2Len/radLenH2O))
+    Twisse8xH2O = calcEq8(thetasqH2O, Twisse8xAl1,m2Len,beta_rel,gamma_rel)
+    Twisse8yH2O = calcEq8(thetasqH2O, Twisse8yAl1,m2Len,beta_rel,gamma_rel)
+
+    #Al Back contribution
+    m3Len = baseSimSetup["MAGNET"][2]["length"]
+    thetasqAl2 = 13.6 * partZ / betap * np.sqrt(m3Len/radLenAl) * (1 + 0.038 * np.log(m3Len/radLenAl))
+    Twisse8xAl2 = calcEq8(thetasqAl2, Twisse8xH2O,m3Len,beta_rel,gamma_rel)
+    Twisse8yAl2 = calcEq8(thetasqAl2, Twisse8yH2O,m3Len,beta_rel,gamma_rel)
+
+  #Plotting PBW Exit distribution vs the PDF produced from the Formalism Equations
+
   #Displays Twiss values and calculated mu and sigma
   #print("\nPre PBW Twiss")
   #plotTwissFit(xinit_filtered/mm,pxinit_filtered,savename+"init",mat,"Pre PBW","X",thick,thetasq,beta_rel,gamma_rel,TwissIx)
   #plotTwissFit(yinit_filtered/mm,pyinit_filtered,savename+"init",mat,"Pre PBW","Y",thick,thetasq,beta_rel,gamma_rel,TwissIy)
   
   print("\nPBW Exit Twiss Calculated")
-  plotTwissFit(xexit_filtered/mm,pxexit_filtered,savename+"texitFiltered",mat,"PBW Exit","X",thick,thetasq,beta_rel,gamma_rel,TwissIx)
+  #plotTwissFit(xexit_filtered/mm,pxexit_filtered,savename+"texitFiltered",mat,"PBW Exit","X",thick,thetasq,beta_rel,gamma_rel,TwissIx)
   #plotTwissFit(yexit_filtered/mm,pyexit_filtered,savename+"texitFiltered",mat,"PBW Exit","Y",thick,thetasq,beta_rel,gamma_rel,TwissIy)
 
-  #Extension to Target
+  #Extension to Target. Needed for compareTargets!
   from plotFit import toTarget
   initTargx = toTarget(TwissIx,"initX")
   intiTargy = toTarget(TwissIy,"initY")
   exitTargx = toTarget(exitxTwf,"exitX")
   exitTargy = toTarget(exityTwf,"exitY")
-  e8Targx = toTarget(Twisse8x,"e8X")
-  e8Targy = toTarget(Twisse8y,"e8Y")
-  e16Targx = toTarget(Twisse16x,"e16X")
-  e16Targy = toTarget(Twisse16y,"e16Y")
+  #e8Targx = toTarget(Twisse8x,"e8X")
+  #e8Targy = toTarget(Twisse8y,"e8Y")
+  #e16Targx = toTarget(Twisse16x,"e16X")
+  #e16Targy = toTarget(Twisse16y,"e16Y")
+  e8TargxReal = toTarget(Twisse8xAl2,"e8XReal")
+  e8TargyReal = toTarget(Twisse8yAl2,"e8YReal")
 
   #Now compare the MiniScatter Target distribution (targxTwissf) to initTarg, exitTarg, e8Targ and e16Targ PDFs
   from plotFit import compareTargets
   #compareTargets(xtarg_filtered/mm,ytarg_filtered/mm,targxTwissf,targyTwissf,initTargx,intiTargy,"Initial",savename,mat)
   #compareTargets(xtarg_filtered/mm,ytarg_filtered/mm,targxTwissf,targyTwissf,exitTargx,exitTargy,"PBW Exit",savename,mat)
   #compareTargets(xtarg_filtered/mm,ytarg_filtered/mm,targxTwissf,targyTwissf,e8Targx,e8Targy,"Eq 8",savename,mat)
-  #compareTargets(xtarg_filtered/mm,ytarg_filtered/mm,targxTwissf,targyTwissf,e16Targx,e16Targx,"Eq 16",savename,mat)
+  #compareTargets(xtarg_filtered/mm,ytarg_filtered/mm,targxTwissf,targyTwissf,e16Targx,e16Targy,"Eq 16",savename,mat)
+  compareTargets(xtarg_filtered/mm,ytarg_filtered/mm,targxTwissf,targyTwissf,e8TargxReal,e8TargyReal,"Eq 8 Real",savename,mat)
   
   return savename, xtarg_filtered_p/mm, ytarg_filtered_p/mm #filter by PDG only
   #return savename, xexit/mm, yexit/mm #bc why?