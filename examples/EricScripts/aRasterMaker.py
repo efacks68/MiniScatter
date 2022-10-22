[WIP] Corrected the ax,y units to be in mm, not meters! And Fx is now in kHz. Edge weighting not yet working, but not crucial.

examples/EricScripts/aRasterMaker.py
@@ -28,21 +28,20 @@ totX = np.zeros([N_t*n_tii*Nparts,2])
 totY = np.zeros([N_t*n_tii*Nparts,2])
 #print(len(totX))
 
-#check units!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-covx = gemtx * np.asarray([[betax,-alphx],[-alphx,(1+alphx**2)/betax]])
-covy = gemty * np.asarray([[betay,-alphy],[-alphy,(1+alphy**2)/betay]])
+covx = gemtx * np.asarray([[betax,-alphx],[-alphx,(1+alphx**2)/betax]]) #[m]
+covy = gemty * np.asarray([[betay,-alphy],[-alphy,(1+alphy**2)/betay]]) #[m]
 
 # raster centre on BEW
 cx_r = 0 # mm
 cy_r = 0 # mm
 cx_r = cx_r * np.ones(N_t) # [mm]
 cy_r = cy_r * np.ones(N_t) # [mm]
-ax0 = 54.65 # [mm]
-ay0 = 18.37 # [mm]
-Fx = 39.953 # [kHz]
-Fy = 28.7051 # [kHz]
+ax0 = 54.65*mm # [mm]
+ay0 = 18.37*mm # [mm]
 ax = ax0 * np.ones(N_t) # [mm]
 ay = ay0 * np.ones(N_t) # [mm]
+Fx = 39.953*1e3 #[kHz]
+Fy = 28.7051*1e3 #[kHz]
 pb = 1
 px = pb/Fx * np.ones(N_t) #[s]
 py = pb/Fy * np.ones(N_t) #[s]
@@ -51,41 +50,37 @@ py = pb/Fy * np.ones(N_t) #[s]
 dt =  np.mean(np.diff(t)) #[s]
 delta_t = np.linspace(0,dt,n_tii) #[s]
 
-i=0
-j=0
-Left = -49
-Right = 49
-Top = 15
-Bottom = -15
-centroids = np.zeros([N_t*n_tii,2])
-print(np.shape(totX))
-
-rng = np.random.default_rng()
-ptsX = rng.multivariate_normal([0,0],covx,size = Nparts)
-##plt.scatter(ptsX[:,0],ptsX[:,1])
+#rng = np.random.default_rng()
+#ptsX = rng.multivariate_normal([0,0],covx,size = Nparts) #[mm]
+#plt.scatter(ptsX[:,0],ptsX[:,1])
 #plt.hist(ptsX)
 #plt.show()
-print(np.covar(ptsX))
+#print(np.covar(ptsX))
 
+i=0
+j=0
+k=0
+#Right = 52*mm
+#Top = 17*mm
+
+centroids = np.zeros([N_t*n_tii,2])
 for jj in range(N_t):
   for ii in range(n_tii):
-
-    tjj_ii = t[jj] + delta_t[ii] 
-    #check units!!!!!!!!!!!!!!
+    tjj_ii = t[jj] + delta_t[ii]
     cx0 = cx_r[jj] + 2 * ax[jj] / math.pi * math.asin(math.sin(2*math.pi/px[jj] * tjj_ii )) #[mm]
     cy0 = cy_r[jj] + 2 * ay[jj] / math.pi * math.asin(math.sin(2*math.pi/py[jj] * tjj_ii )) #[mm]
 
     centroids[i,0] = cx0
     centroids[i,1] = cy0
 
-    #if cx0 < Left or cx0 > Right or cy0 > Top or cy0 < Bottom: #set weight depending on position
-    #  N = Nparts
+    #if cx0 > -Right and cx0 < Right and cy0 < Top and cy0 > -Top: #set weight depending on position
+    #  N = 10
     #  j += 1
     #else:
-    N = Nparts#5 #for later on, write core to csv with difference weight than edge particles for ROOT.
+    N = Nparts #for later on, write core to csv with difference weight than edge particles for ROOT.
+    #  k += 1
 
     rng = np.random.default_rng()
-    #Check units!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 19.10
     ptsX = rng.multivariate_normal([cx0,0],covx,size = N) #mean is [pos,ang]!
     ptsY = rng.multivariate_normal([cy0,0],covy,size = N)
     
@@ -95,7 +90,7 @@ for jj in range(N_t):
       totY[N*i+k,0] = ptsY[k,0]
       totY[N*i+k,1] = ptsY[k,1]
     i +=1
-print(i,j)
+print(i,j,k)
 
 print(np.max(centroids[:,0]),np.max(centroids[:,1]))
 print(np.min(centroids[:,0]),np.min(centroids[:,1]))
@@ -124,8 +119,8 @@ if save:
   part_file.close()
   print(outname)
 
-#plt.plot(totX[:,0], totY[:,0], '.', alpha=0.5)
-plt.hist(totX[:,0],bins='auto')
+plt.plot(totX[:,0], totY[:,0], '.', alpha=0.5)
+#plt.hist(totX[:,0],bins='auto')
 #plt.axis('equal')
 #plt.yaxis.set_minor_locator(MultipleLocator(10))
 plt.grid()